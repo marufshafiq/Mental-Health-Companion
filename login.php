9dd5d7c4c678b79773cc6f53536af3410422ab24
marufshafiq <marufshafiqcsecr@gmail.com>
2025-10-05 13:52:52 +0600

    Authenticaton started

---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Login</title>
+  <link rel="stylesheet" href="login.css">
+</head>
+<body>
+  <div class="container">
+    <h2>Login</h2>
+    <form action="" method="POST">
+      <div class="input-group">
+        <input type="text" name="username" placeholder="Username" required>
+      </div>
+      <div class="input-group">
+        <input type="password" name="password" placeholder="Password" required>
+      </div>
+      <button type="submit" name="login">Login</button>
+    </form>
+    <p class="signup-link">Don't have an account? <a href="signup.php">Sign up</a></p>
+  </div>
+</body>
+</html>
